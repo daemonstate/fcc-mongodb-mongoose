abf74343e09b429889f3187963add6e957020ef0
daemonstate <daemon.state@gmail.com>
2022-06-21 09:25:12 +0600

    connected mongodb and mongoose

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-
+let mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://daemonstate:iujyhh7ZDCv3s9WW@cluster0.qcnhvqg.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
